4f0809ea69b82b7dd446f1e5f117a7ce4fcc5108
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2024-03-05 09:04:31 -0300

    a

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ alert('ola mundo');
  let nomeUsuario = prompt('Qual o seu nome?');
  let anterior = document.querySelector('#texto');
 
+while(nomeUsuario == ""){
+    nomeUsuario = prompt('Qual o seu nome?')
+}
+
 anterior.textContent = nomeUsuario;
