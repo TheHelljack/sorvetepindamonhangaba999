0337ceb24841e1aef9c489e79450a7d3a7baf616
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2024-03-05 08:54:38 -0300

    a

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ alert('ola mundo');
  let nomeUsuario =prompt('Qual o seu nome?');
  let anterior= document.querySelector('#texto');
 
-anterior.textoContent= nomeUsuario;
+anterior.textoContent = nomeUsuario;
