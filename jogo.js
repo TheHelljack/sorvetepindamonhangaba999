178f5c7da4a24fa36e93113ccf965b98218fbdad
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2024-03-15 11:36:09 -0300

    a

---
+++ jogo.js
@@ -0,0 +1,11 @@
+let numeroSecreto = 87;
+
+let chute = prompt('escolha um numero de 0 a 100');
+
+if( numeroSecreto == chute) {
+    alert('Acertou mizera :');
+    console.log('parabens, acertou!')
+}else {
+    alert('errou');
+    console.log('infelizmente errou')
+}
