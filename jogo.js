6747de56d4dd1c0fc6e4c3dd03dc486f4f2be597
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2024-03-15 11:45:19 -0300

    a

--- jogo.js
+++ jogo.js
@@ -1,9 +1,14 @@
 let numeroSecreto = 87;
 
-let chute = prompt('escolha um numero de 0 a 100');
+let chute = '';
+
+while(chute == '') {
+    chute = prompt('escolha um numero de 0 a 100');
+
+}
 
 if( numeroSecreto == chute) {
-    alert('boa moleke :');
+    alert('boa molekee ');
     console.log('parabens, acertou!')
 }else {
     alert('errou marimo');
