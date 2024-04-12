756ef883b4cd526b01a1d3b4bbd3b65f89de6809
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2024-04-12 08:56:24 -0300

    a

--- cont.js
+++ cont.js
@@ -7,6 +7,7 @@ console.log(x);
 botons[x].onclick = function(){
     for (let y = 0; y< botons.length; y++){
     botons[y].classList.remove('ativo');
+    textos[y].classList.remove('ativa');
 }botons[x].classList.add('ativo')
 //botons[x].classList.remove('ativo');
 }
