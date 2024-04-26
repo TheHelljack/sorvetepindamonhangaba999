402c371a1b01bbfadea705c4468c523c828f4135
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2024-04-26 09:16:34 -0300

    s

--- cont.js
+++ cont.js
@@ -26,7 +26,7 @@ let tempoObjetivo4 = new Date('2024-12-13T00:00');
 let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 for (let i = 0; i < contadores.length; i++) {
-contadores[i].textContent = calculaTempo(tempos[i];)
+contadores[i].textContent = calculaTempo(tempos[i]);
 }
 
 function calculaTempo(tempoObjetivo) {
