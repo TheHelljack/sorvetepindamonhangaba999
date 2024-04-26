ade3806ca636c06304523c1ce40ae8a09f06c98e
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2024-04-26 09:09:58 -0300

    s

--- cont.js
+++ cont.js
@@ -25,6 +25,7 @@ let tempoObjetivo4 = new Date('2024-12-13T00:00');
 
 let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
+function () {
 let tempoFinal = tempoObjetivo1 - tempoAtual;
 
 let segundos = Math.floor (tempoFinal/1000);
@@ -37,5 +38,5 @@ minutos %= 60;
 horas %= 24;
 
 contadores [0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos " ;
-
+}
 
